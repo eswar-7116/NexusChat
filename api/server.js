f4359eacf40dcd2391ad904e0594ab755a8e9d70
eswar-7116 <eswardudi06@gmail.com>
2025-01-27 18:07:30 +0530

    Setup express.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
-console.log("Hi")
+import express from 'express'
+
+const app = express()
+
+app.use('/', (req, _, next) => {
+    console.log(`${req.method} request to ${req.url}`)
+    next()
+})
+
+app.listen(5000, '0.0.0.0', () => {
+    console.log("Server started at http://localhost:5000")
+})

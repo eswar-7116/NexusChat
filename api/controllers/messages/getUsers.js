3741e6fdf0b25a1970f5a1d60aae2221136ac046
eswar-7116 <eswardudi06@gmail.com>
2025-01-30 21:42:32 +0530

    Update user retreiving functionality

---
+++ api/controllers/messages/getUsers.js
@@ -0,0 +1,25 @@
+import User from "../../models/User.js";
+
+export default async function getUsers(req, res) {
+    try {
+        const currentUserId = req.user._id;
+        const users = await User.find({
+            $and: [
+                { _id: { $ne: currentUserId } },
+                { isVerified: true }
+            ]
+        }).select("-password");
+        
+        return res.status(200).json({
+            success: true,
+            users
+        });
+    } catch (error) {
+        console.error("Error while getting recent users: "+error);
+        return res.status(500).json({
+            success: false,
+            message: "Error while getting recent users",
+            error: String(error)
+        });
+    }
+}

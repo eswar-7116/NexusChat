b5e738d1827f9c3c74aec283599818b34cdbd578
eswar-7116 <eswardudi06@gmail.com>
2025-01-30 21:43:08 +0530

    Update user retreiving functionality

--- api/routes/messages/routes.js
+++ api/routes/messages/routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 
-import getRecentUsers from "../../controllers/messages/recentUsers.js";
+import getUsers from "../../controllers/messages/getUsers.js";
 import getMessages from "../../controllers/messages/getMessages.js";
 import sendMessage from "../../controllers/messages/sendMessage.js";
 
 const router = Router();
 
-router.get('/recent-users', getRecentUsers);
+router.get('/get-users', getUsers);
 router.get('/get-messages/:id', getMessages);
 router.post('/send/:id', sendMessage);
 

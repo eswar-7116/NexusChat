5d2a4fc349c53fc16ae4dd3293478803fc7270d0
eswar-7116 <eswardudi06@gmail.com>
2025-02-06 21:24:09 +0530

    Correct small mistake

--- api/routes/auth/login.js
+++ api/routes/auth/login.js
@@ -73,7 +73,7 @@ router.post('/login',[
         });
 
         // Set user status to online and save
-        user.status = 'offline';
+        user.status = 'online';
         await user.save();
 
         res.status(200).json({

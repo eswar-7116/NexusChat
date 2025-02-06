db2e6911e9509ba66e3dfc09ff31766eddf4341a
eswar-7116 <eswardudi06@gmail.com>
2025-02-06 22:30:52 +0530

    Rename import

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { KeyRound, LogOut, User, Sun, Moon } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-import { useAuthStore } from '../store/authStore';
+import { useAuthStore } from '../stores/authStore';
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);

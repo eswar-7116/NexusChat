e09ea4b9725aaf3a1120c886d3c2349077191e83
eswar-7116 <eswardudi06@gmail.com>
2025-03-01 16:09:14 +0530

    ⚡Improve SideBar.jsx

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -65,18 +65,17 @@ function SideBar() {
                   alt={user.fullName}
                   className="size-10 sm:size-12 lg:size-12 rounded-full object-cover"
                 />
-                {onlineUsers.includes(user._id) && (
+                { onlineUsers.includes(user) && (
                   <span
-                    className="absolute bottom-0 right-0 size-2.5 sm:size-3 bg-green-500
-                    rounded-full ring-2 ring-zinc-900"
+                    className="absolute bottom-0 right-0 size-2.5 sm:size-3 bg-green-500 rounded-full ring-2 ring-zinc-900"
                   />
                 )}
               </div>
               {/* User Info (Only visible on larger screens) */}
               <div className="hidden lg:block text-left min-w-0 flex-1">
                 <div className="font-medium truncate">{user.fullName}</div>
-                <div className="text-sm text-zinc-400">
-                  {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+                <div className="text-sm text-zinc-400 truncate">
+                  {user.username}
                 </div>
               </div>
             </button>

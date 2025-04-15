cb55c3789465523fc0baa22501728f28b59db3ce
eswar-7116 <eswardudi06@gmail.com>
2025-04-15 21:36:51 +0530

    Correct base URL

--- frontend/src/helpers/axios.js
+++ frontend/src/helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/backend`,
+    baseURL: import.meta.env.VITE_MODE === 'development' ? `${import.meta.env.VITE_BACKEND_URL}/backend` : import.meta.env.VITE_BACKEND_URL,
     withCredentials: true
 });

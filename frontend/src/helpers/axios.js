eb0594751762ee51a7e7183232750aa696dfa9c4
eswar-7116 <eswardudi06@gmail.com>
2025-05-26 12:05:01 +0530

    Prettier formatting and reolve bugs

--- frontend/src/helpers/axios.js
+++ frontend/src/helpers/axios.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_MODE === 'development' ? `${import.meta.env.VITE_BACKEND_URL}/backend` : '/backend',
-    withCredentials: true
+  baseURL:
+    import.meta.env.VITE_MODE !== "production"
+      ? `${import.meta.env.VITE_BACKEND_URL}/backend`
+      : "/backend",
+  withCredentials: true,
 });
